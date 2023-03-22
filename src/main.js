ba777ccbe3e5d14876722a2008b9a62d604d153d
Finestwork <Younes.Espiritu@outlook.com>
2023-03-22 12:20:00 +0800

    Add firebase

--- src/main.js
+++ src/main.js
@@ -4,4 +4,13 @@ import { createPinia } from 'pinia';
 import App from './App.vue';
 import router from './router';
 
+// Your web app's Firebase configuration
+import FirebaseConfig from '../firebase.config.json';
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+
+// Initialize Firebase
+initializeApp(FirebaseConfig);
+
 createApp(App).use(createPinia()).use(router).mount('body');
