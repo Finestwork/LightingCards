9e1f4c8dc30a0debcdea5d0cea9d86a526b07418
Finestwork <Younes.Espiritu@outlook.com>
2023-03-26 09:18:13 +0800

    Add basic template for editing flashcards

--- src/pages/TheFlashCardEditor.vue
+++ src/pages/TheFlashCardEditor.vue
@@ -1,3 +1,118 @@
 <template>
-  <h2>Editing</h2>
+  <div class="create-card">
+    <PlainNavbar />
+
+    <div class="create-card__container container--sm">
+      <BasePlainBreadcrumbs :links="breadcrumbs" />
+      <TheFlashCardCreation
+        @create-card="setCreatedSuccessfully"
+        :items="sets"
+      />
+    </div>
+  </div>
 </template>
+
+<script>
+import PlainNavbar from '@/components/globals/navbars/PlainNavbar.vue';
+import BasePlainBreadcrumbs from '@/components/globals/breadcrumbs/BasePlainBreadcrumbs.vue';
+import TheFlashCardCreation from '@/components/single-instance/TheFlashCardCreation.vue';
+
+// Helpers
+import FlashcardHelper from '@/assets/js/helpers/flashcard-helper';
+
+// NPM
+import { useToast } from 'vue-toastification';
+
+export default {
+  components: {
+    PlainNavbar,
+    TheFlashCardCreation,
+    BasePlainBreadcrumbs
+  },
+  data() {
+    return {
+      breadcrumbs: [
+        {
+          text: 'Home',
+          to: { name: 'Landing' }
+        },
+        {
+          text: 'Edit set',
+          to: { name: 'EditFlashcard' }
+        }
+      ],
+      sets: FlashcardHelper.createDefaultCards(2)
+    };
+  },
+  methods: {
+    setCreatedSuccessfully() {
+      useToast().success('Successfully created', {
+        timeout: 6000
+      });
+      this.$router.push({ name: 'Landing' });
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+@use 'sass:map';
+@use '../assets/scss/1-settings/css-properties/font-size/major-second';
+@use '../assets/scss/2-tools/mixins/css-properties/margin';
+@use '../assets/scss/2-tools/mixins/css-properties/width-and-height';
+@use '../assets/scss/2-tools/mixins/css-properties/font-size';
+@use '../assets/scss/2-tools/mixins/css-properties/all-properties';
+@use '../assets/scss/4-layouts/containers';
+
+// prettier-ignore
+.create-card {
+  &__container {
+    width: 90%;
+    @include margin.top((
+        xsm: 50
+    ));
+    :deep(.plain-breadcrumbs){
+      @include margin.bottom((
+          xsm:35
+      ));
+      .breadcrumb__link{
+        @include font-size.responsive((
+            lg: map.get(major-second.$scale, 3)
+        ));
+      }
+      .breadcrumb__icon{
+        @include margin.horizontal((
+            lg: 7
+        ));
+      }
+    }
+    :deep(.card__set-controls){
+      @include all-properties.init((
+          md: (
+              display: flex
+          )
+      ));
+      @include margin.top((
+          md: 45
+      ));
+
+      .set-control__form-fields{
+        @include width-and-height.set((
+            md: (width: 45% ),
+            lg: (width: 40%),
+        ));
+        @include margin.right((
+            md: 35
+        ));
+      }
+
+      .set-control__form-switches{
+        @include width-and-height.set((
+            md: (width: 45% ),
+            lg: (width: 60%),
+        ));
+      }
+    }
+  }
+}
+</style>
